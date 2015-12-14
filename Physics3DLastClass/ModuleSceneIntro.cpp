6921bdd42b90dce52f28675df2e4101e11011358
kellazo <gilli11519@gmail.com>
2015-12-14 22:38:37 +0100

    Circuit building

--- Physics3DLastClass/ModuleSceneIntro.cpp
+++ Physics3DLastClass/ModuleSceneIntro.cpp
@@ -8,7 +8,21 @@
 
 ModuleSceneIntro::ModuleSceneIntro(Application* app, bool start_enabled) : Module(app, start_enabled)
 {
-	vel,red,blue = 0;
+	temp = 0.1f;
+	posY_block = 7.0f;
+	psupport = prod = pbob = NULL;
+	posX_bob = 0.0f;
+	tempb = 0.1f;
+	pl1 = pl2 = 12.0f;
+	pr1 = pr2 = -12.0f;
+	tl1 = tl2 = tr1 = tr2 = t1 = t2 = tw1 = tw2 = two1 = two2 = 0.1f;
+	pc_left1 = pc_left2 = pc_right1 = pc_right2 = NULL;
+	circuit_x = 50.0f;
+	py1 = py2 = -5.0f;
+	pxw1 = 12.0f;
+	pxw2 = 1.0f;
+	pxwo1 = 6.0f;
+	pxwo2 = -6.0f;
 }
 
 ModuleSceneIntro::~ModuleSceneIntro()
@@ -25,52 +39,170 @@ bool ModuleSceneIntro::Start()
 	//longitud circuit
 	float road_width = 20.0f;
 	float road_height = 2.0f;
-	float circuit_x = 50.0f;
-	recta1.size.Set(road_width, road_height, circuit_x);
-	recta1.SetPos(0, road_height / 2, circuit_x / 2);
-	App->physics->AddBody(recta1, 0)->collision_listeners.add(this);
+	//float circuit_x = 50.0f;
 
-	recta2.size.Set(road_width, road_height, 10);
-	recta2.SetPos(0, road_height / 2, 56);
-	App->physics->AddBody(recta2, 0)->collision_listeners.add(this);
+	//pole position & first block
+	road_start.size.Set(road_width, road_height, circuit_x);
+	road_start.SetPos(0, road_height / 2, circuit_x / 2);
+	App->physics->AddBody(road_start, 0)->collision_listeners.add(this);
 
 	vec3 vec(0, 0, 1);
-	poste1.height = 10.0f;
-	poste1.SetRotation(90, vec);
-	poste1.radius = 1.0f;
-	poste1.SetPos(-9, 7, 56);
-	App->physics->AddBody(poste1, 0);
+	pole_left.height = 10.0f;
+	pole_left.SetRotation(90, vec);
+	pole_left.radius = 1.0f;
+	pole_left.SetPos(-9, 7, circuit_x / 2);
+	App->physics->AddBody(pole_left, 0);
 
 
-	poste2.height = 10.0f;
-	poste2.SetRotation(90, vec);
-	poste2.radius = 1.0f;
-	poste2.SetPos(9, 7, 56);
-	App->physics->AddBody(poste2, 0);
+	pole_right.height = 10.0f;
+	pole_right.SetRotation(90, vec);
+	pole_right.radius = 1.0f;
+	pole_right.SetPos(9, 7, circuit_x / 2);
+	App->physics->AddBody(pole_right, 0);
 
-	cartell.size.Set(road_width, 3, 2);
-	cartell.SetPos(0, 13.5, 56);
-	App->physics->AddBody(cartell, 0)->collision_listeners.add(this);
+	poster.size.Set(road_width, 3, 2);
+	poster.SetPos(0, 13.5, circuit_x / 2);
+	App->physics->AddBody(poster, 0)->collision_listeners.add(this);
 
-	
-	
-	curva2.height = 2.0f;
-	curva2.SetRotation(90, vec);
-	curva2.radius = 7.0f;
-	curva2.SetPos(0, 1, 70);
-	App->physics->AddBody(curva2, 0);
+	road_path1.size.Set(road_width, road_height, circuit_x);
+	road_path1.SetPos(0, road_height / 2, circuit_x / 2 + circuit_x);
+	App->physics->AddBody(road_path1, 0)->collision_listeners.add(this);
 
 	block.size.Set(5, 5, 5);
-	//block.SetPos(0, 13, 20);
+	block.SetPos(0, 20, circuit_x);
 	pbblock = App->physics->AddBody(block, 0);
 
+	// Simple Pendulum
 
-	floor.size.Set(500.0f, 1.0f, 500.0f);
-	floor.SetPos(0, -1, 0);
-	floor.color = Black;
+	support.size.Set(10.0f, 2.0f, 4.0f);
+	support.SetPos(0, 20, circuit_x + (circuit_x / 2));
+	psupport = App->physics->AddBody(support, 0);
 
-	//floor.size.Set(5, 3, 1);
-	//floor.SetPos(0.0f, 4.5f, 20.0f);
+	rod.height = 12.0f;
+	rod.SetRotation(90, vec);
+	rod.radius = 1.0f;
+	rod.SetPos(0, 20, ((circuit_x + circuit_x / 2)));
+	prod = App->physics->AddBody(rod, 1);
+
+	vec3 anchorA(0, -1.5, 1);
+	vec3 anchorB(6, 0, 0);
+
+	vec3 axisA(0, 0, 1);
+	vec3 axisB(0, 0, 1);
+
+	//App->physics->AddConstraintP2P(*(psupport),*(prod), anchorA,anchorB);
+	App->physics->AddConstraintHinge(*(psupport), *(prod), anchorA, anchorB, axisA, axisB);
+
+	bob.radius = 2.0f;
+	bob.SetPos(-10, 5, ((circuit_x + circuit_x / 2)));
+	pbob = App->physics->AddBody(bob, 1);
+
+	vec3 anchorAr(-7, 0, 0);
+	vec3 anchorBb(0, -1, 0);
+	App->physics->AddConstraintHinge(*(prod), *(pbob), anchorAr, anchorBb, axisA, axisB);
+
+	//Road blocks motion on X axis
+	road_path2.size.Set(road_width, road_height, circuit_x);
+	road_path2.SetPos(0, road_height / 2, circuit_x / 2 + circuit_x + circuit_x);
+	App->physics->AddBody(road_path2, 0)->collision_listeners.add(this);
+
+	c_left1.size.Set(3, 2, 3);
+	c_left1.SetPos(12, 3, circuit_x / 2 + circuit_x + 10);
+	App->physics->AddBody(c_left1, 0);
+
+	c_left2.size.Set(3, 2, 3);
+	c_left2.SetPos(12, 3, circuit_x / 2 + circuit_x + 20);
+	App->physics->AddBody(c_left2, 0);
+
+	c_right1.size.Set(3, 2, 3);
+	c_right1.SetPos(-12, 3, circuit_x / 2 + circuit_x + 15);
+	App->physics->AddBody(c_right1, 0);
+
+	c_right2.size.Set(3, 2, 3);
+	c_right2.SetPos(-12, 3, circuit_x / 2 + circuit_x + 25);
+	App->physics->AddBody(c_right2, 0);
+
+	pc_left1 = App->physics->AddBody(c_left1, 0);
+	pc_left2 = App->physics->AddBody(c_left2, 0);
+	pc_right1 = App->physics->AddBody(c_right1, 0);
+	pc_right2 = App->physics->AddBody(c_right2, 0);
+
+	//more roads
+
+	road1.size.Set(road_width, road_height, circuit_x);
+	road1.SetPos(0, road_height / 2, circuit_x / 2 + circuit_x * 2);
+	App->physics->AddBody(road1, 0)->collision_listeners.add(this);
+
+	road2.size.Set(road_width, road_height, circuit_x);
+	road2.SetPos(0, road_height / 2, circuit_x / 2 + circuit_x * 3);
+	App->physics->AddBody(road2, 0)->collision_listeners.add(this);
+
+	road3.size.Set(road_width, road_height, circuit_x);
+	road3.SetPos(0, road_height / 2, circuit_x / 2 + circuit_x * 4);
+	App->physics->AddBody(road3, 0)->collision_listeners.add(this);
+
+	road4.size.Set(road_width, road_height, circuit_x);
+	road4.SetPos(0, road_height / 2, circuit_x / 2 + circuit_x * 5);
+	App->physics->AddBody(road4, 0)->collision_listeners.add(this);
+
+	road5.size.Set(road_width, road_height, circuit_x);
+	road5.SetPos(0, road_height / 2, circuit_x / 2 + circuit_x * 6);
+	App->physics->AddBody(road5, 0)->collision_listeners.add(this);
+
+	road6.size.Set(road_width, road_height, circuit_x);
+	road6.SetPos(0, road_height / 2, circuit_x / 2 + circuit_x * 7);
+	App->physics->AddBody(road6, 0)->collision_listeners.add(this);
+
+	road7.size.Set(road_width, road_height, circuit_x);
+	road7.SetPos(0, road_height / 2, circuit_x / 2 + circuit_x * 8);
+	App->physics->AddBody(road7, 0)->collision_listeners.add(this);
+
+	road8.size.Set(road_width, road_height, circuit_x);
+	road8.SetPos(0, road_height / 2, circuit_x / 2 + circuit_x * 9);
+	App->physics->AddBody(road8, 0)->collision_listeners.add(this);
+
+	road9.size.Set(road_width, road_height, circuit_x);
+	road9.SetPos(0, road_height / 2, circuit_x / 2 + circuit_x * 10);
+	App->physics->AddBody(road9, 0)->collision_listeners.add(this);
+
+	road10.size.Set(road_width, road_height, circuit_x);
+	road10.SetPos(0, road_height / 2, circuit_x / 2 + circuit_x * 11);
+	App->physics->AddBody(road10, 0)->collision_listeners.add(this);
+
+	//blocks from ground and up
+
+	blockground1.size.Set(5, 5, 5);
+	blockground1.SetPos(3, -4, circuit_x / 2 + circuit_x * 2);
+	pblockground1 = App->physics->AddBody(blockground1, 0);
+
+	blockground2.size.Set(5, 5, 5);
+	blockground2.SetPos(-3, -4, circuit_x / 2 + circuit_x * 2);
+	pblockground2 = App->physics->AddBody(blockground2, 0);
+
+	//2 wall on middle
+
+	wall_mid1.size.Set(2, 8, 10);
+	wall_mid1.SetPos(12, 6, circuit_x / 2 + circuit_x * 2 + 25);
+	pwall_mid1 = App->physics->AddBody(wall_mid1, 0);
+
+	wall_mid2.size.Set(2, 8, 10);
+	wall_mid2.SetPos(-12, 6, circuit_x / 2 + circuit_x * 2 + 25);
+	pwall_mid2 = App->physics->AddBody(wall_mid2, 0);
+
+	// 2 wall out
+
+	wall_outl.size.Set(2, 8, 8);
+	wall_outl.SetPos(12, 4, circuit_x / 2 + circuit_x * 3);
+	pwall_outl = App->physics->AddBody(wall_outl, 0);
+
+	wall_outr.size.Set(2, 8, 8);
+	wall_outr.SetPos(-12, 4, circuit_x / 2 + circuit_x * 3);
+	pwall_outr = App->physics->AddBody(wall_outr, 0);
+
+	//flippers wall
+
+	//hole road
+
 
 	floor_sensor = App->physics->AddBody(floor, 0.0f);
 	floor_sensor->SetAsSensor(true);
@@ -113,24 +245,180 @@ update_status ModuleSceneIntro::Update(float dt)
 		
 	}*/
 
-	recta1.Render();
-	recta2.Render();
-	poste1.Render();
-	poste2.Render();
-	cartell.Render();
-	curva2.Render();
+	road_start.Render();
+	road_path1.Render();
+	pole_left.Render();
+	pole_right.Render();
+	poster.Render();
 
-	for (red = 0; red <= 20; red++)
-	{
-		
-		pbblock->GetTransform(&(block.transform));
-		//block.SetPos(0, red, 20);
-		//pbblock->Push(red, 0, 0);
-		block.Render();
-		
-	}
-	
-	//block.color.Set(100, 0, 50);
+
+	//velocity  y  block
+	if (posY_block >= 20)
+		temp = temp * -1;
+	if (posY_block <= 6)
+		temp = temp * -1;
+	posY_block += temp;
+	block.SetPos(0, posY_block, 50.0f);
+	pbblock->SetPos(0, posY_block, 50.0f);
+	block.Render();
+
+	//Render Simple Pendulum
+	support.Render();
+	prod->GetTransform(&rod.transform);
+	rod.Render();
+	//velocity on axisX of Bob
+	pbob->GetTransform(&bob.transform);
+	/*if (posX_bob >= 7)
+	tempb = tempb * -1;
+	if (posX_bob <= -7)
+	tempb = tempb * -1;
+	posX_bob += tempb;
+	bob.SetPos(posX_bob, 5, ((50 + 50 / 2)));
+	pbob->SetPos(posX_bob, 5, ((50 + 50 / 2)));*/
+
+	bob.Render();
+
+	road_path2.Render();
+
+	//Motion of Cube blocks on X axis
+	pc_left1->GetTransform(&c_left1.transform);
+	pc_left2->GetTransform(&c_left2.transform);
+	pc_right1->GetTransform(&c_right1.transform);
+	pc_right2->GetTransform(&c_right2.transform);
+
+	if (pl1 >= 12)
+		tl1 = tl1 * -1;
+	if (pl1 <= -12)
+		tl1 = tl1 * -1;
+	pl1 += tl1;
+
+	c_left1.SetPos(pl1, 3, circuit_x / 2 + circuit_x + 10);
+	pc_left1->SetPos(pl1, 3, circuit_x / 2 + circuit_x + 10);
+
+	if (pl2 >= 12)
+		tl2 = tl2 * -1;
+	if (pl2 <= -12)
+		tl2 = tl2 * -1;
+	pl2 += tl2;
+
+	c_left2.SetPos(pl2, 3, circuit_x / 2 + circuit_x + 20);
+	pc_left2->SetPos(pl2, 3, circuit_x / 2 + circuit_x + 20);
+
+	if (pr1 >= -12)
+		tr1 = tr1 * -1;
+	if (pr1 <= 12)
+		tr1 = tr1 * -1;
+	pr1 += tr1;
+
+	c_right1.SetPos(pr1, 3, circuit_x / 2 + circuit_x + 15);
+	pc_right1->SetPos(pr1, 3, circuit_x / 2 + circuit_x + 15);
+
+	if (pr2 >= -12)
+		tr2 = tr2 * -1;
+	if (pr2 <= 12)
+		tr2 = tr2 * -1;
+	pr2 += tr2;
+
+	c_right2.SetPos(pr2, 3, circuit_x / 2 + circuit_x + 25);
+	pc_right2->SetPos(pr2, 3, circuit_x / 2 + circuit_x + 25);
+
+
+	c_left1.Render();
+	c_left2.Render();
+	c_right1.Render();
+	c_right2.Render();
+
+
+	//more roads
+	road1.Render();
+	road2.Render();
+	road3.Render();
+	road4.Render();
+	road5.Render();
+	road6.Render();
+	road7.Render();
+	road8.Render();
+	road9.Render();
+	road10.Render();
+
+
+	//blocks from ground and up
+
+	//TODO: fer forats al terra i fer que puguin els blocks  perque si hi ha un objecte fisic xoca i no travessa
+	/*pblockground1->GetTransform(&blockground1.transform);
+	pblockground2->GetTransform(&blockground2.transform);
+
+	if (py1 >= 10)
+	t1 = t1 * -1;
+	if (py1 <= -4)
+	t1 = t1 * -1;
+	py1 += t1;
+	blockground1.SetPos(3, py1, circuit_x / 2 + circuit_x * 2);
+	pblockground1->SetPos(-3, py1, circuit_x / 2 + circuit_x * 2);
+	blockground1.Render();
+
+	if (py2 >= 10)
+	t2 = t2 * -1;
+	if (py2 <= -4)
+	t2 = t2 * -1;
+	py2 += t2;
+	blockground2.SetPos(-3, py2, circuit_x / 2 + circuit_x * 2);
+	pblockground2->SetPos(-3, py2, circuit_x / 2 + circuit_x * 2);
+	blockground2.Render();*/
+
+
+	//2 wall on middle
+	//pwall_mid1->GetTransform(&wall_mid1.transform);
+	//pwall_mid2->GetTransform(&wall_mid2.transform);
+
+	/*if (pxw1 >= 12)
+	tw1 = tw1 * -1;
+	if (pxw1 <= -12)
+	tw1 = tw1 * -1;
+	pxw1 += tw1;
+
+	wall_mid1.SetPos(pxw1, 6, circuit_x / 2 + circuit_x * 2 + 25);
+	*/pwall_mid1 = App->physics->AddBody(wall_mid1, 0);
+
+	wall_mid1.Render();
+
+
+	/*if (pxw2 >= 1)
+	tw2 = tw2 * -1;
+	if (pxw2 <= -10)
+	tw2 = tw2 * -1;
+	pxw2 += tw2;
+	wall_mid2.SetPos(pxw2, 6, circuit_x / 2 + circuit_x * 2 + 25);
+	pwall_mid2 = App->physics->AddBody(wall_mid2, 0);*/
+	wall_mid2.Render();
+
+	// 2 wall out
+	/*pwall_outl->GetTransform(&wall_outl.transform);
+	pwall_outr->GetTransform(&wall_outr.transform);
+
+	if (pxwo1 >= 10)
+	two1 = two1 * -1;
+	if (pxwo1 <= -4)
+	two1 = two1 * -1;
+	pxwo1 += two1;
+	wall_outl.SetPos(pxwo1, 3, circuit_x / 2 + circuit_x * 3);
+	pwall_outl = App->physics->AddBody(wall_outl, 0);
+	wall_outl.Render();
+
+	if (pxwo2 >= 10)
+	two2 = two2 * -1;
+	if (pxwo2 <= -4)
+	two2 = two2 * -1;
+	pxwo2 += two2;
+	wall_outr.SetPos(pxwo2, 3, circuit_x / 2 + circuit_x * 3);
+	pwall_outr = App->physics->AddBody(wall_outr, 0);
+	wall_outr.Render();*/
+
+	//flippers wall
+
+	//hole road
+
+
 	
 
 	return UPDATE_CONTINUE;
--- Physics3DLastClass/ModuleSceneIntro.h
+++ Physics3DLastClass/ModuleSceneIntro.h
@@ -42,14 +42,59 @@ public:
 	PhysMotor3D* left_wheel;
 	PhysMotor3D* right_wheel;
 
-	//circuit
+	//------CIRCUIT
+
+	//long circuit
+	float circuit_x;
+
+	//pole & block
 	PhysBody3D* pbblock;
-	Cube recta1, recta2, cartell, block;
-	Cylinder curva1, curva2, poste1, poste2;
+	Cube road_start, road_path1, poster, block;
+	Cylinder pole_left, pole_right;
+	float posY_block, temp;
 
-	int vel, red, blue;
+	//Simple Pendulum
+	PhysBody3D* psupport;
+	PhysBody3D* prod;
+	PhysBody3D* pbob;
+	Cube support;
+	Cylinder rod;
+	Sphere bob;
+	float posX_bob, tempb;
 
+	//road with blocks x axis motion
+	Cube c_left1, c_left2, c_right1, c_right2, road_path2;
+	float pl1, pl2, pr1, pr2, tl1, tl2, tr1, tr2;
+	PhysBody3D* pc_left1;
+	PhysBody3D* pc_left2;
+	PhysBody3D* pc_right1;
+	PhysBody3D* pc_right2;
 
+	//more road path
+	Cube road1, road2, road3, road4, road5, road6, road7, road8, road9, road10;
+
+	//blocks from ground and up
+	Cube blockground1, blockground2;
+	PhysBody3D* pblockground1;
+	PhysBody3D* pblockground2;
+	float py1, py2, t1, t2;
+
+	//wall on middle
+	Cube wall_mid1, wall_mid2;
+	PhysBody3D* pwall_mid1;
+	PhysBody3D* pwall_mid2;
+	float pxw1, pxw2, tw1, tw2;
+
+	//wall out
+	Cube wall_outl;
+	PhysBody3D* pwall_outl;
+	Cube wall_outr;
+	PhysBody3D* pwall_outr;
+	float pxwo1, pxwo2, two1, two2;
+
+	//flippers wall
+
+	//hole road
 
 
 	p2List<PhysBody3D*> pbpieces;
